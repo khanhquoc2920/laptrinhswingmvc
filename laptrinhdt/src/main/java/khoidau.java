04e22d755b9074930568040318054c501170472d
khanhquoc2920 <khanhquoc2920@gmail.com>
2021-02-25 16:03:19 +0700

    Cong viec lap trinh 2

--- laptrinhdt/src/main/java/khoidau.java
+++ laptrinhdt/src/main/java/khoidau.java
@@ -8,13 +8,13 @@
  *
  * @author Admin
  */
-public class testmain {
+public class khoidau {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("xinchao");
+      System.out.println("xinchao");
     }
     
 }
